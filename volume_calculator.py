fbe0f29da3dfc5511f8c71107fd8f5917cded4ba
Mar Canet Sola <mar.canet@gmail.com>
2023-12-24 04:19:27 +0200

    Update volume_calculator.py

--- volume_calculator.py
+++ volume_calculator.py
@@ -2,8 +2,8 @@
 
 '''
 VOLUME CALCULATION STL MODELS
-Author: Mar Canet (mar.canet@gmail.com) - September 2012
-Description: Calculate volume and mass of STL models (binary and ASCII).
+Author: Mar Canet (mar.canet@gmail.com) - September 2012-2023
+Description: Calculate volume and mass of STL models (binary and ASCII), NIfTI, and DICOM files.
 '''
 
 import struct
@@ -149,26 +149,119 @@ class STLUtils:
         areaCm2 = area / 100
         print("Total area:", areaCm2, "cm^2")
         return areaCm2
+        
+class VolumeDataProcessor:
+    def __init__(self, file_path, file_type):
+        self.file_path = file_path
+        self.file_type = file_type
+        self._import_dependencies()
 
+    def _import_dependencies(self):
+        global nib, pydicom, measure, np, mesh
+
+        try:
+            import nibabel as nib
+        except ImportError:
+            print("Nibabel is not installed. Install it for NIfTI file support.")
+
+        try:
+            import pydicom
+        except ImportError:
+            print("Pydicom is not installed. Install it for DICOM file support.")
+
+        try:
+            from skimage import measure
+        except ImportError:
+            print("Scikit-Image is not installed. Install it for image processing support.")
+
+        try:
+            import numpy as np
+        except ImportError:
+            print("Numpy is not installed. Install it for numerical operations.")
+
+        try:
+            import stl.mesh as mesh
+        except ImportError:
+            print("numpy-stl is not installed. Install it for STL mesh support.")
+
+    def read_volume_data(self):
+        if self.file_type == "nii":
+            try:
+                img = nib.load(self.file_path)
+                return img.get_fdata()
+            except ImportError:
+                print("Nibabel is not installed. Install it for NIfTI file support.")
+                sys.exit(1)
+        elif self.file_type == "dcm":
+            try:
+                ds = pydicom.dcmread(self.file_path)
+                return ds.pixel_array
+            except ImportError:
+                print("Pydicom is not installed. Install it for DICOM file support.")
+                sys.exit(1)
+        else:
+            raise ValueError("Unsupported file type")
+
+    def generate_isosurface(self, data):
+        verts, faces, _, _ = measure.marching_cubes(data, level=0.5)
+        surface_mesh = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+        for i, f in enumerate(faces):
+            for j in range(3):
+                surface_mesh.vectors[i][j] = verts[f[j],:]
+        return surface_mesh
+    
+    # Calculate surface area of the mesh
+    def calculate_surface_area(self, surface_mesh):
+        area = 0.0
+        for f in surface_mesh.vectors:
+            p0, p1, p2 = f
+            a = np.linalg.norm(p1 - p0)
+            b = np.linalg.norm(p2 - p1)
+            c = np.linalg.norm(p0 - p2)
+            s = (a + b + c) / 2.0
+            area += (s * (s - a) * (s - b) * (s - c)) ** 0.5
+        return area
+
+    # Calculate volume of the mesh
+    def calculate_volume(self, surface_mesh):
+        volume = 0.0
+        for f in surface_mesh.vectors:
+            p1, p2, p3 = f
+            v321 = p3[0] * p2[1] * p1[2]
+            v231 = p2[0] * p3[1] * p1[2]
+            v312 = p3[0] * p1[1] * p2[2]
+            v132 = p1[0] * p3[1] * p2[2]
+            v213 = p2[0] * p1[1] * p3[2]
+            v123 = p1[0] * p2[1] * p3[2]
+            volume += (-v321 + v231 + v312 - v132 - v213 + v123) / 6.0
+        return abs(volume)
+        
 def main():
     parser = argparse.ArgumentParser(description='Calculate volume or surface area of STL models.')
-    parser.add_argument('filename', help='Path to the STL file')
+    parser.add_argument('filename', help='Path to the file')
     parser.add_argument('calculation', choices=['volume', 'area'], help='Choose between calculating volume or surface area')
     parser.add_argument('--unit', choices=['cm', 'inch'], default='cm', help='Unit for the volume calculation (default: cm)')
-    parser.add_argument('--material', type=int, choices=range(1, 19), help='Material ID for mass calculation')
+    parser.add_argument('--material', type=int, choices=range(1, 19),default=2, help='Material ID for mass calculation')
+    parser.add_argument('--filetype', choices=['stl', 'nii', 'dcm'], default='stl', help='Type of the input file: stl, nii, dcm')
 
     args = parser.parse_args()
 
-    mat = materialsFor3DPrinting()
-    material_mass = mat.get_material_mass(args.material)
-
-    mySTLUtils = STLUtils()
-    mySTLUtils.loadSTL(args.filename)
-
-    if args.calculation == 'volume':
-        mySTLUtils.calculateVolume(args.unit, material_mass)
-    elif args.calculation == 'area':
-        mySTLUtils.surf_area()
+    if args.filetype == 'stl':
+        mySTLUtils = STLUtils()
+        mySTLUtils.loadSTL(args.filename)
+        if args.calculation == 'volume':
+            material_mass = materialsFor3DPrinting().get_material_mass(args.material)
+            mySTLUtils.calculateVolume(args.unit, material_mass)
+        elif args.calculation == 'area':
+            mySTLUtils.surf_area()
+    elif args.filetype in ['nii', 'dcm']:
+        volumeDataProcessor = VolumeDataProcessor(args.filename, args.filetype)
+        data = volumeDataProcessor.read_volume_data()
+        surface_mesh = volumeDataProcessor.generate_isosurface(data)
+        if args.calculation == 'volume':
+            print('Volume:', volumeDataProcessor.calculate_volume(surface_mesh), 'units^3')
+        elif args.calculation == 'area':
+            print('Surface area:', volumeDataProcessor.calculate_surface_area(surface_mesh), 'units^2')
 
 if __name__ == '__main__':
     main()
